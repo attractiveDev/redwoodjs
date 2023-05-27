104113db083840e2385c9834e85a6cae662cc431
victorperkins <victorperkins02262@gmail.com>
2023-05-27 15:04:23 +0900

    redwoodjs project

---
+++ web/src/components/ArticleCell/ArticleCell.js
@@ -0,0 +1,25 @@
+import Article from "../Article/Article"
+export const QUERY = gql`
+  query ArticleQuery($id: Int!) {
+    article: post(id: $id) {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => (
+  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+)
+
+export const Success = ({ article, id }) => {
+  console.log("id", id)
+  console.log("article", article)
+  return <Article article={article} />
+}
---
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -0,0 +1,42 @@
+import { Link, routes } from '@redwoodjs/router'
+import { useAuth } from 'src/auth'
+
+const BlogLayout = ({ children }) => {
+  const { isAuthenticated, currentUser, logOut } = useAuth()
+  return (
+    <>
+      <header>
+        <h1>
+          <Link to={routes.home()}>Redwood Blog</Link>
+        </h1>
+        {
+          isAuthenticated ? (
+            <div>
+              <span>Logged is as {currentUser.email}</span>{' '}
+              <button type='button' onClick={logOut}>
+                Logout
+              </button>
+            </div>
+          ) :
+          <Link to={routes.login()}>Login</Link>
+        }
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            <li>
+              <Link to={routes.contact()}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default BlogLayout
